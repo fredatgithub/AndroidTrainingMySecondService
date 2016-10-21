bfbcb9c711037a553be4fbf48e08cfad2b1b2ff5
fred <domaine75@gmail.com>
2016-10-21 14:34:33 +0100

    update files

--- app/src/main/java/ltm/service/ActivityLaunched.java
+++ app/src/main/java/ltm/service/ActivityLaunched.java
@@ -8,22 +8,25 @@ import android.widget.Toast;
 
 public class ActivityLaunched extends Activity {
 	private Handler _handler = null;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		Toast.makeText( this, "ActivityLaunched", Toast.LENGTH_LONG ).show();
 
-		_handler = new Handler() {
+		_handler = new Handler()
+        {
 			@Override
-			public void handleMessage(Message msg) {
+			public void handleMessage(Message msg)
+            {
+				//Toast.makeText(this, (msg.toString()), Toast.LENGTH_LONG).show();
 				finish();
 			}
 		};
 		new Minuterie().start();
 	}
-	
+
 	private class Minuterie extends Thread {
 		@Override
 		public void run() {
@@ -35,6 +38,6 @@ public class ActivityLaunched extends Activity {
 			_handler.sendMessage( _handler.obtainMessage( 0, null ) );
 			super.run();
 		}
-		
+
 	}
 }
